adcc88d1efb9176a7d4c09ff21399aea72bbdcbc
VenkateshBhat <venkatesh@circuitects.com>
2016-07-07 18:32:08 +0530

    arduino basic analog read-out sketch

--- Libraries/example/exapmle.ino
+++ Libraries/example/exapmle.ino
@@ -1,86 +1,116 @@
-#include <ads1262.h>
+/*Wiring to your Arduino
+ example.ino
+ this example gives differential voltage across the AN0 And AN1 in mV
+ Hooking-up with the Arduino
+----------------------
+|ads1262 pin label| Pin Function         |Arduino Connection|
+|-----------------|:--------------------:|-----------------:|
+| DRDY            | Data ready Output pin|  D6              |             
+| MISO            | Slave Out            |  D12             |
+| MOSI            | Slave In             |  D11             |
+| SCLK            | Serial Clock         |  D13             |
+| CS              | Chip Select          |  D7              |
+| START           | Start Conversion     |  D5              | 
+| PWDN            | Power Down/Reset     |  D4              |
+| DVDD            | Digital VDD          |  +5V             |
+| DGND            | Digital Gnd          |  Gnd             |
+| AN0-AN9         | Analog Input         |  Analog Input    |
+| AINCOM          | Analog input common  |                  |
+| AVDD            | Analog VDD           |  -               |
+| AGND            | Analog Gnd           |  -               |
+-----------------------------------------------------------------
+*/
 #include <SPI.h>
+#include <SoftwareSerial.h>
+#include <math.h>
+#include <ads1262.h>
 
-ads1262 ADS1292;
+#define PGA 1                     // Programmable Gain = 1
+#define VREF 2.50                 // Internal reference of 2.048V
+#define VFSR VREF/PGA             
+#define FSR (((long int)1<<23)-1)  
 
-volatile char SPI_RX_Buff[150];
-volatile char EEG_Ch2_Data[10];
+ads1262 PC_ADS1262;                     // class
+
+float volt_V=0;
+float volt_mV=0;
+volatile int i;
+volatile char SPI_RX_Buff[10];
+volatile long ads1262_rx_Data[10];
 volatile static int SPI_RX_Buff_Count = 0;
 volatile char *SPI_RX_Buff_Ptr;
 volatile int Responsebyte = false;
-volatile unsigned int pckt =0 , buff=0,t=0 , j1=0,j2=0;
-unsigned long ueegtemp = 0,Pkt_Counter=0;
-signed long seegtemp=0;
-volatile int i;
-int count=0;
+volatile signed long sads1262Count = 0;
+volatile signed long uads1262Count=0;
+double resolution;
+
 
 
 void setup() 
 {
   // initalize the  data ready and chip select pins:
-  pinMode(ADS1262_DRDY_PIN, INPUT);
-  pinMode(ADS1262_CS_PIN, OUTPUT);
-  pinMode(ADS1262_START_PIN, OUTPUT);
-  pinMode(ADS1262_PWDN_PIN, OUTPUT);
-  
-  //initalize ADS1292 slave
-  ADS1292.ads1262_Init();
+  pinMode(ADS1262_DRDY_PIN, INPUT);                  //data ready input line
+  pinMode(ADS1262_CS_PIN, OUTPUT);                   //chip enable output line
+  pinMode(ADS1262_START_PIN, OUTPUT);               // start 
+  pinMode(ADS1262_PWDN_PIN, OUTPUT);                // Power down output   
+
   Serial.begin(9600);
+  //initalize ADS1292 slave
+  PC_ADS1262.ads1262_Init();                      // initialise ads1262
+  Serial.println("ads1262 Initialised successfully....");
+
  }
 
 void loop() 
 {
   volatile int i,data;
 
-  if( !(millis()%1000) && count)
-  {
-  //  Serial.print("DRDY low: "); 
- // Serial.println(count); 
-  count= 0;
-  }
   
- if((digitalRead(ADS1262_DRDY_PIN)) == LOW)
+ if((digitalRead(ADS1262_DRDY_PIN)) == LOW)               // monitor Data ready(DRDY pin)
   {  
-    SPI_RX_Buff_Ptr = ADS1292.ads1262_Read_Data();
-    Responsebyte = true ;
-   // Serial.print("DRDY low: "); 
-   count++;
+    SPI_RX_Buff_Ptr = PC_ADS1262.ads1262_Read_Data();      // read 6 bytes conversion register
+    Responsebyte = true ; 
   }
 
   if(Responsebyte == true)
   {
-    /*Serial.print("start Time: ");
-    time = millis();
-    //prints time since program started
-    Serial.println(time);
-    */   
-    for(i = 0; i <12; i++)
+    for(i = 0; i <5; i++)
     {
-      SPI_RX_Buff[SPI_RX_Buff_Count++] = *(SPI_RX_Buff_Ptr + i);
+      SPI_RX_Buff[SPI_RX_Buff_Count++] = *(SPI_RX_Buff_Ptr + i);              
     }
     Responsebyte = false;
   }
   
-  if(SPI_RX_Buff_Count >= 12)
+  if(SPI_RX_Buff_Count >= 5)
   {     
-    pckt = 0; j1=0;   j2=0;
 
-    EEG_Ch2_Data[0]= (unsigned char)SPI_RX_Buff[0];
-    EEG_Ch2_Data[1]= (unsigned char)SPI_RX_Buff[1];
-    EEG_Ch2_Data[2]= (unsigned char)SPI_RX_Buff[2];
-    EEG_Ch2_Data[3]= (unsigned char)SPI_RX_Buff[3];
 
-   
-    //seegtemp = -89485;
-  
-     ueegtemp = (unsigned long) ((EEG_Ch2_Data[3]<<24)|(EEG_Ch2_Data[2]<<16)|(EEG_Ch2_Data[1]<<8)|EEG_Ch2_Data[0]);
- //    ueegtemp = (unsigned long) (ueegtemp<<8);
-     seegtemp = (signed long) (ueegtemp);
- //    seegtemp = (signed long) (seegtemp>>8); 
+    ads1262_rx_Data[0]= (unsigned char)SPI_RX_Buff[1];  // read 4 bytes adc count
+    ads1262_rx_Data[1]= (unsigned char)SPI_RX_Buff[2];
+    ads1262_rx_Data[2]= (unsigned char)SPI_RX_Buff[3];
+    ads1262_rx_Data[3]= (unsigned char)SPI_RX_Buff[4];
 
-   Serial.println( seegtemp,8);
-    }
+ 
+     uads1262Count = (signed long) (((unsigned long)ads1262_rx_Data[0]<<24)|((unsigned long)ads1262_rx_Data[1]<<16)|(ads1262_rx_Data[2]<<8)|ads1262_rx_Data[3]);//get the final out by shifting
+     sads1262Count = (signed long) (uads1262Count); 
+     resolution = (double)((double)2.5/pow(2,31));
+    // Serial.print(resolution,15);
+     volt_V      = (resolution)*(float)sads1262Count;
+     volt_mV   =   volt_V*1000;
+     Serial.print("Read volt");
+     Serial.print(" : ");
+     Serial.print(volt_V);
+     Serial.print(" V ,");
+     Serial.print(volt_mV);
+     Serial.println(" mV");
+ 
+   }
     
   SPI_RX_Buff_Count = 0;
 
+}
+
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
